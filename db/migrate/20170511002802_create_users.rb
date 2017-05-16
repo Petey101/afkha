addc4503f2cbd8c22a7f213a3b98ac92575ad3ae
Petey101 <pleong101@live.com>
2017-05-16 10:16:17 -0400

    Welcome page started

--- db/migrate/20170511002802_create_users.rb
+++ db/migrate/20170511002802_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :points
+      t.string :name, null: false, unique: true
+      t.integer :points , default: 0
 
       t.timestamps null: false
     end
